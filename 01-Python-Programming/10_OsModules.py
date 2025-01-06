679a88a0f25b27c2e2e2a60c0bf218dc88aa0092
Hassan Mahmood <hassanmahmudawan@gmail.com>
2025-01-06 19:49:03 +0500

    python: OSModule

---
+++ 01-Python-Programming/10_OsModules.py
@@ -0,0 +1,20 @@
+import os
+
+print(os.name)                        # ?
+print(os.path.exists("main.py"))
+
+# Get working Directory
+abs_path = os.getcwd()                      # Absolute Path.
+print(os.path.join(abs_path, "main.py"))
+
+print(os.path.abspath("main.py"))
+
+if  os.path.exists("created.txt"):
+    os.mkdir("created")
+    
+    
+print(os.listdir())
+
+os.rename("instructions.txt", "Instruction.txt")    #Source -> Destination
+
+os.rmdir("created") 
