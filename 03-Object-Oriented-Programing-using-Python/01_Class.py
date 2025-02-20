ef2d0b654c7cc457accde7b2709ce663e0a4be4d
Hassan Mahmood <hassanmahmudawan@gmail.com>
2025-02-20 13:20:13 +0500

    Concept Learning Started

--- 03-Object-Oriented-Programing-using-Python/01_Class.py
+++ 03-Object-Oriented-Programing-using-Python/01_Class.py
@@ -1,6 +1,10 @@
 class Employee:
+    # Class Attributes.
     COMPANY="Allied bank Limited"
-    WEBSITE="www.abl.com.pk"
+    
+    @classmethod
+    def company_details(cls):
+        return cls.COMPANY
     
     # Special Method to Initialze Instance Attributes
     def __init__(self, name="Hassan", grade="MG-10"):
@@ -8,8 +12,22 @@ class Employee:
         self.name = name
         self.grade = grade
         
+    # Method
+    def employee_info(self):
+        return self.name, self.grade
+    
+        
 
 if __name__ == "__main__":
     emp = Employee(name="Hassan Mahmood")    
-    print(emp_obj.name)
+    
+    print(emp.name)
+    
+    name, grade = emp.employee_info()
+    print(name, grade, sep="\n")
+    
+    print(Employee.company_details())
+    emp.COMPANY = "Allied Bank Limited, Lahore."
+    print(emp.company_details())
+
         
